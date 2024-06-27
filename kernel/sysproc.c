baade7c4c8c0407b4a53e71556f0714da24b8585
zzn <a2064968308@gmail.com>
2024-06-27 20:55:08 +0800

    lab2-syscall-trace

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -99,9 +99,9 @@ sys_uptime(void)
 uint64
 sys_trace(void)
 {
-  int mask;
+  uint64 mask;
 
-  if(argint(0, &mask) < 0)
+  if(argaddr(0, &mask) < 0)
     return -1;
   
   myproc()->trace_mask = mask;
